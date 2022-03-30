4aa3ef5ff804cbc1d27958ceaefc9b39713d41f8
IvanMatveev <vanadiiii42@gmail.com>
2022-03-30 22:53:11 +0400

    add amazing hanoi tower game)

---
+++ src/main/java/me/imatveev/reqursion/HanoiTower.java
@@ -0,0 +1,13 @@
+package me.imatveev.reqursion;
+
+public class HanoiTower {
+    public static void doTower(int topN, char from ,char inter, char to) {
+        if (topN == 1) {
+            System.out.println("Disk 1 from " + from + " to " + to);
+        } else {
+            doTower(topN - 1, from, to, inter); //from --> inter
+            System.out.println("Disk " + topN + " from " + from + " to " + to);
+            doTower(topN - 1, inter, from, to); //inter --> to
+        }
+    }
+}
