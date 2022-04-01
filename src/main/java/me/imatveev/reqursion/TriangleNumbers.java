1c07eee4d179d8c652a9af452de2d732b9f8fc61
IvanMatveev <vanadiiii42@gmail.com>
2022-04-01 17:48:44 +0400

    getTriangleNumbersSum1(10000) = 50005000, time - 572 getTriangleNumbersSum2(10000) = 50005000, time - 137;
    recursion lose)

--- src/main/java/me/imatveev/reqursion/TriangleNumbers.java
+++ src/main/java/me/imatveev/reqursion/TriangleNumbers.java
@@ -2,6 +2,14 @@ package me.imatveev.reqursion;
 
 public class TriangleNumbers {
     public static int getTriangleNumbersSum(int num) {
-        return (num == 1) ? 1 : num + getTriangleNumbersSum(num);
+        return (num == 1) ? 1 : num + getTriangleNumbersSum(num - 1);
+    }
+
+    public static int getTriangleNumbersSum2(int num) {
+        int sum = 0;
+        while (num >= 1) {
+            sum += num--;
+        }
+        return sum;
     }
 }
