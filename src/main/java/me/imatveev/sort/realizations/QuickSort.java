4da0f9c34b58645b79c21c45ba66b2ee8b7be11f
IvanMatveev <vanadiiii42@gmail.com>
2022-04-18 15:56:07 +0400

    add median's searching in QuickSort

--- src/main/java/me/imatveev/sort/realizations/QuickSort.java
+++ src/main/java/me/imatveev/sort/realizations/QuickSort.java
@@ -16,24 +16,38 @@ public class QuickSort<T extends Comparable<T>> extends AbstractSortingAlgorithm
 
     private void recSort(T[] data, Comparator<T> comparator, int left, int right) {
         if (right > left) {
-            T rightElem = data[right];
+            int medianIdx = findMedian(data, left, right, comparator);
+            T element = data[medianIdx];
 
-            final int middle = findMiddle(data, rightElem, comparator, left, right);
+            final int middle = findMiddle(data, element, comparator, left, right);
 
             recSort(data, comparator, left, middle - 1);
             recSort(data, comparator, middle + 1, right);
         }
     }
 
-    private int findMiddle(T[] data, T rightElem, Comparator<T> comparator, int left, int right) {
+    private int findMedian(T[] data, int left, int right, Comparator<T> comparator) {
+        int median = (left + right) / 2;
+
+        if (comparator.compare(data[left], data[median]) > 0) {
+            swap(data, left, median);
+        }
+        if (comparator.compare(data[median], data[right]) > 0) {
+            swap(data, median, right);
+        }
+
+        return median;
+    }
+
+    private int findMiddle(T[] data, T element, Comparator<T> comparator, int left, int right) {
         int left2 = left - 1;
         int right2 = right;
 
         while (true) {
             //find the smaller than rightElem
-            while (comparator.compare(data[++left2], rightElem) < 0) ;
+            while (comparator.compare(data[++left2], element) < 0) ;
             //find the bigger than rightElem
-            while (right2 > 0 && comparator.compare(data[--right2], rightElem) > 0) ;
+            while (right2 > 0 && comparator.compare(data[--right2], element) > 0) ;
 
             if (left2 >= right2) {
                 break;
